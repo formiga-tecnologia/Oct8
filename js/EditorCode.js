51452948cf6bb1158f17062a9c33c7030719f222
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2021-05-31 00:43:13 -0300

    update

--- js/EditorCode.js
+++ js/EditorCode.js
@@ -3,4 +3,9 @@
 const codeArea = document.getElementById("codetext");
 codeArea.addEventListener('input',()=>{
     console.log(codeArea.value)
+    fetch("https://oct8lang.herokuapp.com/")
+    .then(response => response.text())
+    .then(data =>{
+        console.log(data)
+    })
 })
