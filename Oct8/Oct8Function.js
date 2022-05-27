1442789050267669416e642abec57773b3800f55
Felipe <cataofelipe@gmail.com>
2022-05-27 16:34:03 -0300

    update documet

--- Oct8/Oct8Function.js
+++ Oct8/Oct8Function.js
@@ -10,3 +10,19 @@ function Oct8RemovePropries(element,propried)
 {
     element.classList.remove(propried)
 }
+ /*
+ Element = elemento alvo 
+ infinity = False ou true para efeito infinito
+ X =  int (1 - 9) quantidade / posição do movimento 
+ backEffect = true or false Se ele vai e volta
+ full = true or false Determina se ele vai ir ate 100% ou até x+40
+
+ */
+function oct8CreateAnimation_ToX(element,infinity,X,backEffect,full)
+{
+    element.className += " move-toX-"+X+" ";
+    if(infinity == true)
+    {
+        element.className += " inifinity"
+    }
+}
