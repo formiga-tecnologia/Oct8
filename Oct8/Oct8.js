127219ad15417b09cf2ceb5e31c85778490505e2
Felipe <cataofelipe@gmail.com>
2022-05-27 14:53:45 -0300

    update

--- Oct8/Oct8.js
+++ Oct8/Oct8.js
@@ -3,16 +3,14 @@ let Gravitybasic = 10;
 let BasicWind = 5;
 
 function GravityGlobal() {
-    let cbeItens =document.getElementsByClassName("cbe-action")
+    let cbeItens = document.getElementsByClassName("cbe-action")
     for (let index = 0; index < cbeItens.length; index++) {
-    cbeItens.item(index).style.marginTop =Gravitybasic+"vh"
-    if(cbeItens.item(index).classList.contains("cbe-rotate"))
-    {
-        cbeItens.item(index).style.transform  = "rotate("+BasicWind+"deg)";
+        cbeItens.item(index).style.marginTop = Gravitybasic + "vh"
+        if (cbeItens.item(index).classList.contains("cbe-rotate")) {
+            cbeItens.item(index).style.transform = "rotate(" + BasicWind + "deg)";
+        }
     }
-    
+    Gravitybasic += 3;
+    BasicWind += 80;
 }
-    Gravitybasic+=3;
-    BasicWind+=80;
-}
-setInterval(GravityGlobal,1000);
+setInterval(GravityGlobal, 1000);
