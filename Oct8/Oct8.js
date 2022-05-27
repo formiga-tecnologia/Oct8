a63af28fe077bf4d42e8344addeb8a6704e6a8ea
Felipe <cataofelipe@gmail.com>
2022-05-27 03:34:23 -0300

    add gravity functions

--- Oct8/Oct8.js
+++ Oct8/Oct8.js
@@ -1,1 +1,12 @@
-//Physical rules 
+//Physical rules  of OCT8 Web System
+let Gravitybasic = 10;
+
+function GravityGlobal() {
+    let cbeItens =document.getElementsByClassName("cbe-action")
+    for (let index = 0; index < cbeItens.length; index++) {
+    cbeItens.item(index).style.marginTop =Gravitybasic+"vh"
+    
+}
+    Gravitybasic+=10;
+}
+setInterval(GravityGlobal,1000);
