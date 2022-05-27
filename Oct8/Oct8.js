edfb28ae43a45ddb8313a2ccd586bf3d82906f74
Felipe <cataofelipe@gmail.com>
2022-05-27 03:42:33 -0300

    add rotation functions

--- Oct8/Oct8.js
+++ Oct8/Oct8.js
@@ -1,12 +1,18 @@
 //Physical rules  of OCT8 Web System
 let Gravitybasic = 10;
+let BasicWind = 5;
 
 function GravityGlobal() {
     let cbeItens =document.getElementsByClassName("cbe-action")
     for (let index = 0; index < cbeItens.length; index++) {
     cbeItens.item(index).style.marginTop =Gravitybasic+"vh"
+    if(cbeItens.item(index).classList.contains("cbe-rotate"))
+    {
+        cbeItens.item(index).style.transform  = "rotate("+BasicWind+"deg)";
+    }
     
 }
-    Gravitybasic+=10;
+    Gravitybasic+=3;
+    BasicWind+=80;
 }
 setInterval(GravityGlobal,1000);
