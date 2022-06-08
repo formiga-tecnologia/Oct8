6f475d20e913df2df51dd8fc1a2c7180915d654d
Felipe <cataofelipe@gmail.com>
2022-06-08 01:03:34 -0300

    modify debug ,mode

--- Oct8/Oct8Function.js
+++ Oct8/Oct8Function.js
@@ -141,9 +141,7 @@ function Oct8CPlayMove(element,oldpos,newpos,axis)
 async function _animatePrivate()
 {
     _element.classList.remove(_axis+"-"+_oldPosition)
-    console.log("REMOVER :"+_axis+"-"+_oldPosition)
     _element.classList += " "+_axis+"-"+_NewPosition
-    console.log("ADICIONAR: "+_axis+"-"+_oldPosition)
 
 }
 function Oct8NewElementContainer(id,AppendElementId,typeContainerProp)
