950ba0a187ab758ad6e03c2632774a0b87206f63
Felipe <cataofelipe@gmail.com>
2022-06-13 01:23:25 -0300

    add Position Element

--- Oct8/Oct8Engine.js
+++ Oct8/Oct8Engine.js
@@ -3,14 +3,30 @@
    DESIGNER PRODUCT: FELIPE CATÃO 
    CREATION DATE: 11/06/2021 */
 
-class Oct8{
-    /* COntainers Created */
-    CreateContainerBody(elementInsertId,Id){
-        Oct8NewElementContainer(Id,elementInsertId,'cbe cbe-on')
+class Oct8 {
+    /* Containers Created */
+    CreateContainerBody(elementInsertId, Id) {
+        Oct8NewElementContainer(Id, elementInsertId, 'cbe cbe-on')
+    }
+
+    CreateContainerElement(elementInsertId, Id) {
+        Oct8NewElementContainer(Id, elementInsertId, 'elb  elb-on')
+    }
+
+    CreateContainerSquareElement(elementInsertId, Id) {
+        Oct8NewElementContainer(Id, elementInsertId, 'sse  sse-on')
     }
 
     /* Modify Props */
-    ModifySize(getElement,axis,value,OldValue){
-        Oct8ChangeSizes(getElement,axis,value,OldValue)
+    ModifySize(getElement, axis, value, OldValue) {
+        Oct8ChangeSizes(getElement, axis, value, OldValue)
+    }
+    AddPositionToElement(Element, axis, pos) {
+        if (axis == 'X') {
+            oct8AddX(Element, pos)
+        }
+        else {
+            oct8AddY(Element, pos)
+        }
     }
 }
