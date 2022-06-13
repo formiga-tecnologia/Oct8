deef72041fcdb30309cbe916a76238f5897b54ab
Felipe <cataofelipe@gmail.com>
2022-06-13 01:53:55 -0300

    update

--- Oct8/Oct8Engine.js
+++ Oct8/Oct8Engine.js
@@ -4,6 +4,17 @@
    CREATION DATE: 11/06/2021 */
 
 class Oct8 {
+    /* CREATE ENVS VARS -- FOR MECHANICAL OF GAME */
+    constructor(id,element)
+    {
+        this.X = null;
+        this.Y =null;
+        this.id =id
+        this.element = element
+
+    }
+    /*  Add Transitions */
+
     /* Containers Created */
     CreateContainerBody(elementInsertId, Id) {
         Oct8NewElementContainer(Id, elementInsertId, 'cbe cbe-on')
@@ -27,6 +38,17 @@ class Oct8 {
         }
         else {
             oct8AddY(Element, pos)
+        }
+    }
+    
+    /* Modify Dynamic position */
+    PlayMove(element, oldpos, newpos, axis){
+        Oct8CPlayMove(element, oldpos, newpos, axis)
+        if(axis == 'X'){
+            this.X=newpos
+        }
+        if(axis == 'Y'){
+            this.Y =  newpos
         }
     }
 }
