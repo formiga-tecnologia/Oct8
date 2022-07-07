d78275c2c67eb227c79e5258f48dcf5775deac22
Felipe <cataofelipe@gmail.com>
2022-07-06 23:38:21 -0300

    Update

---
+++ Oct8Web/public/app.js
@@ -0,0 +1,13 @@
+
+var fileinput = document.getElementById("file-input")
+
+var ref = firebase.storage().ref('Project_01')
+fileinput.onchange = function (event) {
+    var arquivo = event.target.files[0]
+    ref.child('arquivo').put(arquivo).then( snapshot =>{
+        console.log('snapshot',snapshot)
+        ref.child('arquivo').getDownloadURL().then(url =>{
+            console.log('string para download',url)
+        })
+    })
+}
