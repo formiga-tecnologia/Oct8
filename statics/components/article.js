8b91a3940f0d4459b5e6ee25b35c9488f09f93c7
Felipe <cataofelipe@gmail.com>
2022-09-10 16:13:35 -0300

    update pyshics

--- statics/components/article.js
+++ statics/components/article.js
@@ -2,11 +2,21 @@ import Oct8 from '../../Oct8/Oct8.js'
 
 export default function CreateNewArticle(x,y,id,values,tile,link){
     var Article =  new Oct8(id,x,y,45,35,"elb elb-off","article",true)
-    InsertValues(values,id,tile,link)
+    InsertValues(values,id,tile,link)  
+    Article.Pyshics.ReverseForce =false
     document.getElementById(id).addEventListener('click',()=>{
         //Article.CreateAnimationEvent(Article.PropsElement.MoveX,10,1,"+")
         Article.Pyshics.SetDynamics(1,90,0,12)
-        Article.Pyshics.CreateMoveForce(Article.GetElementId(),Article.OctObj,true)
+        Article.Pyshics.CreateMoveForce(Article.GetElementId(),Article.OctObj,Article.Pyshics.ReverseForce)
+        if(Article.Pyshics.ReverseForce == false)
+        {
+            Article.Pyshics.ReverseForce = true
+        }
+        else
+        {
+            Article.Pyshics.ReverseForce = false
+        }
+
     },false)
 }
 
