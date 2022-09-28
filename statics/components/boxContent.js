292bfc4c912d410fade2409f74ece2941294d330
Felipe <cataofelipe@gmail.com>
2022-09-27 23:33:37 -0300

    create new componets

---
+++ statics/components/boxContent.js
@@ -0,0 +1,10 @@
+import Oct8 from "../../Oct8/Oct8.js";
+import Oct8Events from "../../Oct8/Oct8Events.js";
+
+export default function CreateBocContent(id,title,content){
+
+}
+
+function InsertValues(id,title,content,oct8pass){
+
+}
