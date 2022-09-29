86c49c8cb5e82c72ce6f8a4b3179b525deede8d2
Felipe <cataofelipe@gmail.com>
2022-09-28 23:52:03 -0300

    create box container oct8

--- statics/components/boxContent.js
+++ statics/components/boxContent.js
@@ -2,7 +2,7 @@ import Oct8 from "../../Oct8/Oct8.js";
 import Oct8Events from "../../Oct8/Oct8Events.js";
 
 export default function CreateBocContent(id,title,content){
-
+    var BoxContentStart = new Oct8(id,0,0,20,40,'elb elb-off','material',true)
 }
 
 function InsertValues(id,title,content,oct8pass){
