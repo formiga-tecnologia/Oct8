d2d13e3b4a24a55da5df3428dfd030054db2bbd5
Felipe <cataofelipe@gmail.com>
2022-09-29 01:07:34 -0300

    update quickStart

--- statics/components/boxContent.js
+++ statics/components/boxContent.js
@@ -1,10 +1,13 @@
 import Oct8 from "../../Oct8/Oct8.js";
 import Oct8Events from "../../Oct8/Oct8Events.js";
 
-export default function CreateBocContent(id,title,content){
-    var BoxContentStart = new Oct8(id,0,0,20,40,'elb elb-off','material',true)
+export default function CreateBoxContent(id,title,content){
+    var BoxContentStart = new Oct8(id,2,2,120,80,'elb elb-off','materialPage',true)
+    InsertValues(id,title,content,BoxContentStart)
 }
 
 function InsertValues(id,title,content,oct8pass){
-
-}
+    document.getElementById(id).innerHTML = "<article><h1>"+title+"</h1>"+
+    "<p>"+content+"</p>"+
+    "</article>"
+} 
