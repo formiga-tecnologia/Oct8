db8b46ae60a7e1b4f01b0ab58895bd84c5ea5e1a
Felipe <cataofelipe@gmail.com>
2023-02-08 00:36:18 -0300

    update landing page

---
+++ statics/mainpage.js
@@ -0,0 +1,5 @@
+import Oct8 from "../Oct8/Oct8.js";
+
+var OctEngine = new Oct8();
+
+OctEngine.CreateAnimationEvent(document.getElementById("squareBasic"),OctEngine.PropsElement.Rotate,10,0.3,"-",Infinity)
