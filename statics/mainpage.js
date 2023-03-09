e608ac3062e3802f0ad407fb6b16c3062f3af8de
Felipe <cataofelipe@gmail.com>
2023-03-08 23:04:59 -0300

    update

--- statics/mainpage.js
+++ statics/mainpage.js
@@ -1,4 +1,5 @@
 import Oct8 from "../Oct8/Oct8.js";
+import Oct8Events from "../Oct8/Oct8Events.js";
 
 var OctEngine = new Oct8();
 
@@ -6,6 +7,7 @@ var OctEngine = new Oct8();
 OctEngine.CreateAnimationEvent(document.getElementById("squareBasic"),OctEngine.PropsElement.Rotate,80,-1,"-",Infinity)
 OctEngine.CreateObjectFactory(ArticleComp,"ArtigoPrincipal")
 OctEngine.CreateObjectFactory(ElementoAnimado,"renderAnimado")
+OctEngine.CreateObjectFactory(ExCodigo,"ExemploCodigo")
 
 
 //Criação com artigo de componente
@@ -23,11 +25,20 @@ function ElementoAnimado(conteudo){
     OctEngine.CreateAnimationCssEvent(conteudo[1],animacao,2,4,"infinite",true,false)
 }
 
+function ExCodigo(conteudo)
+{
+    document.getElementById("ArticleDocs").innerHTML=conteudo[0]
+    document.getElementById("CodeReview_oct8").innerHTML=conteudo[1]
+}
+
 //Conteudos que os componetes podem recebrer de forma padrão
 var CompArray = {
     ElementoArtigo_01: ["Artigo01","https://th.bing.com/th/id/R.67e6dab06cdfda3e8fef1b22b6f9f468?rik=zEbv6UNd7vooBw&pid=ImgRaw&r=0","O Oct8 possui sistema de mini fabricas,onde cada fabrica produz um elemento totalmente replicavel e costumizavel,sem muita enrolação, simples e direta ao ponto.","boxfac"],
     ElementoArtigo_02: ["Artigo02","https://th.bing.com/th/id/OIP.icyD4BfpPgBruE9_ok5MxgHaHa?pid=ImgDet&rs=1","Desenvolvimento de suas paginas web ganham mais vida com o nosso framework , que alem da vlocidade, ela te proprociona mais liberdade criativa para desenvolver do seu jeito","boxcode"],
     ElementoArtigo_03: ["Artigo03","https://th.bing.com/th/id/R.8ce8ffc984c80bfd339958aa1fa904c0?rik=P4vw5d%2bEqTXKQQ&riu=http%3a%2f%2fcdn.onlinewebfonts.com%2fsvg%2fimg_545542.png&ehk=J9tzxqGHV9Iz68q6GetB57lWMmnTW0tR0FZDn8t4FGI%3d&risl=&pid=ImgRaw&r=0","A legibilidade do codigo é simplesmente facil, alem da sua curva de aprendizado ser altamente baixa podendo no mesmo dia começar a produzir seu proprio projeto web.","boxdev"]
+}
+var CodigoArray  ={
+    CodigoExemplo_01:["<h1>Como criar uma fabrica</h1></br><h4>As fabricas são eventos pre determinados que fazem a montagem do seus futuros componentes em Oct8.</h4>","<h6>import Oct8 from '../Oct8/Oct8.js';<br/>&nbsp &nbsp var OctEngine = new Oct8();</br> &nbsp &nbsp OctEngine.CreateObjectFactory(ArticleComp,'ArtigoPrincipal')</h6>"]
 }
 
 
@@ -37,3 +48,5 @@ OctEngine.AppendObjectFacyotyTo("ArtigoPrincipal",CompArray.ElementoArtigo_02)
 OctEngine.AppendObjectFacyotyTo("ArtigoPrincipal",CompArray.ElementoArtigo_03)
 
 //Cirado de elemento 
+
+OctEngine.AppendObjectFacyotyTo("ExemploCodigo",CodigoArray.CodigoExemplo_01)
