1858286e39fb3cee6091a619b7de523822cc333d
Felipe <cataofelipe@gmail.com>
2023-03-17 21:31:22 -0300

    created main start obj

---
+++ main.js
@@ -0,0 +1,9 @@
+import Oct8 from "./Oct8/Oct8.js"
+import Oct8Events from "./Oct8/Oct8Events.js"
+
+class Oct8Base{
+    oct_Object = new Oct8()
+    main(){
+        // Create your function here
+    }
+}
