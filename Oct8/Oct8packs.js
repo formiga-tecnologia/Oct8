d03496e962262c0c2171cb1b751ce67a6670588d
FelipeKatao <cataofelipe@gmail.com>
2023-09-30 14:06:14 -0300

    create packs styles

--- Oct8/Oct8packs.js
+++ Oct8/Oct8packs.js
@@ -1,16 +1,38 @@
 
 class oct8Pack{
     _State_pack = ""
-    RenderPack(Atribute){
-        console.log(Atribute)
-        this._State_pack = Atribute
-        return Atribute
+    Object_pack = ""
+    Style_pack = ""
+    Var_packs = ""
+    PacksProps(){
+        let index = 1
+        this.Var_packs.forEach(element => {
+            if(String(this.Object_pack).includes("$"+index)){
+                this.Object_pack = String(this.Object_pack).replace("{$"+index+"}",element)
+            }
+            index+=1
+        });
     }
-     ReadPack(PackUrl,SaveState){
+    RenderStyle(){
+        let index = 1
+        this.Style_pack.forEach(styles =>{
+            if(String(this.Object_pack).includes("#"+index)){
+                this.Object_pack = String(this.Object_pack).replace("{#"+index+"}",styles)
+            }
+            index+=1
+        })
+    }
+    RenderPack(Element){
+        Element.innerHTML+= this.Object_pack
+    }
+     ReadPack(PackUrl){
         fetch(PackUrl).then(Response=>{
             return Response.json()
         }).then(data=>{
             this._State_pack = data
+            this.Object_pack = data[0]["Pack"]["component"]
+            this.Var_packs = data[0]["Pack"]["Prop"]
+            this.Style_pack = data[0]["Pack"]["style"]
         })
     }
 }
