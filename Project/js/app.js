eb8a6d448308074fc7e48eea1e557273b5c1dd35
FelipeKatao <cataofelipe@gmail.com>
2024-03-22 00:04:55 -0300

    update app.py

--- Project/js/app.js
+++ Project/js/app.js
@@ -1,7 +1,8 @@
 const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
     const win = new BrowserWindow({
-
+      width: 800,
+      height: 600,
       fullscreen:true
     })
   
