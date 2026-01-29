02a15c302fed1187a94bc78b0568a85306ef76a9
FelipeKatao <cataofelipe@gmail.com>
2026-01-28 23:07:04 -0300

    Ajustes no projeto

--- public/js/quickStart.js
+++
@@ -1,7 +0,0 @@
-import Oct8 from '../../Oct8/Oct8.js'
-import CreateBoxContent from '../components/boxContent.js'
-
-CreateBoxContent("HowStart","O que é Oct8?","O Oct8 é um mini framework que gerencia de forma facil e intuitiva o seu front end. A responsabilidade do Oct8 é criar um sistema interativo para suas paginas web, extremamente leve e facil de ser utilizado, ele executa o paradigma de design 'No Scrool page' onde toda informação fica em uma unica pagina, abaixo um exemplo de como criar um pequeno elemento com Oct8:"+ 
-"<br/><div class='boxExample'> import Oct8 from .Oct8/oct8.js"+  
-" </br> var AboutArticle = new Oct8(id,0,0,'infinity',0,'elb elb-on','material',true)  </div>")
-
