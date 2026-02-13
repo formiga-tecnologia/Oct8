b5cd43b5e1b3f98bcb340c5dc54edf1df24ac059
FelipeKatao <cataofelipe@gmail.com>
2026-02-12 23:32:37 -0300

    Update

--- Demo/component/Banner.js
+++ Demo/component/Banner.js
@@ -1,23 +1,13 @@
 import { Oct8 } from "../../TS/Oct8/Oct.js"
 class Banner{
-    constructor(props){
+    constructor(props={}){
         this.props=props
     }
-    build(){
-        Oct8.Reaction.create("teste",30)
 
+    build(){
         return `
-        <div class='banner'>
-            <img class='logoBanner' src='../image/Oct8Logo2026.png' >
-            <div class='compBanner' >
-            ${Oct8.Reaction.inject("teste")}
-                <h1> ${this.props.Titulo} </h1>
-                <h3> ${this.props.Conteudo} </h3>
-                <button oct-event='click_teste'> Acessar </button>
-                <button> Acessar documentacao </button
-            </div>
-            </div>
-            <div oct-style='page:white'> 
+        <div>
+            ${Oct8.Factory.ExistProp("Titulo",this.props,(p)=>{return`<h1> ${p} </h1>`})} 
         </div>
         `
     }
--- Demo/main.js
+++ Demo/main.js
@@ -1,5 +1,18 @@
-import { MenuBlog } from "./component/menu.js";
 import { Oct8 } from "../TS/Oct8/Oct.js";
+//Componentes
+import { MenuBlog } from "./component/menu.js";
+import { Banner } from "./component/Banner.js";
+
+Oct8.Factory.register("Banner_big",Banner)
 
 Oct8.Factory.register("MenuPrinc",MenuBlog)
 Oct8.Factory.render("MenuPrinc","#app",{menu:["Home","Referencia","Templates","Blog"]})
+
+
+//Paginas
+import { HomePage } from "./templates/Home.js";
+Oct8.Factory.register("Home",HomePage)
+
+//So para teste
+Oct8.Factory.render("Home","#app")
+HomePage.buildPage()
