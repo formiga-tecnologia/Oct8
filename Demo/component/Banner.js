3a0ab8cec817e0caffa3fca23ad3504ba5975d0b
FelipeKatao <cataofelipe@gmail.com>
2026-02-13 09:29:49 -0300

    Ajustes na tela deDemostração

--- Demo/component/Banner.js
+++ Demo/component/Banner.js
@@ -3,11 +3,22 @@ class Banner{
     constructor(props={}){
         this.props=props
     }
-
+    codeExemple(code){
+        let v = String(code)
+        return `<div class='codeEx'> <nav>Codigo js</nav> <p> ${v} <p> </div>`
+    }
     build(){
         return `
-        <div>
+        <div oct-css='banner ${Oct8.Factory.ExistProp("style",this.props,(p)=>{return p})}'>
+            <article>
+            ${Oct8.Factory.ExistProp("img",this.props,(p)=>{return`<img src='${p}' width='500'>`})} 
             ${Oct8.Factory.ExistProp("Titulo",this.props,(p)=>{return`<h1> ${p} </h1>`})} 
+            ${Oct8.Factory.ExistProp("Subtitulo",this.props,(p)=>{return`<h3> ${p} </h3>`})} 
+            ${Oct8.Factory.ExistProp("Button",this.props,(p)=>{return`<button> ${p} </button>`})} 
+            </article>
+            <p>
+            ${Oct8.Factory.ExistProp("Code",this.props,this.codeExemple)} 
+            </p>
         </div>
         `
     }
--- Demo/templates/Home.js
+++ Demo/templates/Home.js
@@ -4,7 +4,18 @@ class HomePage{
     this.prop = prop
   }
   static buildPage(){
-    Oct8.Factory.render("Banner_big","#page",{Titulo:"teste"})
+    Oct8.Factory.render("Banner_big","#page",{Titulo:"Oct8 2026",style:"big orange",
+      Subtitulo:"Framework web para criação e gerenciamento de UI interativa",
+      Button:"Acessar Documentação",img:"../image/Oct8Logo2026.png"})
+
+     Oct8.Factory.render("Banner_big","#page",{Titulo:"Construa componentes mais rapido!",style:"big whiteRange flex",
+      Subtitulo:"Com Oct8 se tornou mais intuitivo contruir componentes com Js com legibilidade e flexibilidade.",
+      Button:"Acessar Documentação",Code:`
+        < r base> <br>
+           < base> < /base> </br>
+        < /r base>
+        
+      `})
   }
    build(){
     return `
